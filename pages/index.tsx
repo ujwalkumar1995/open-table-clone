a2fdc0b1591cedc4db2f6a6f729c9b1471619767
Ujwal Kumar <ujwalkumar95@gmail.com>
2023-07-29 19:21:43 +0530

    Add homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-import styles from '../styles/Home.module.css'
+import { Inter } from 'next/font/google'
+ 
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <main className={styles.main}>
-        <h1 className='text-blue-400 text-7xl'>Hello World</h1>
-      </main>
-    </>
+    <main>
+    </main>
   )
 }

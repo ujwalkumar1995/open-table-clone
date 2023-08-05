b59c3fe7e008d40feda0b1efc4e90be8b583e20f
Ujwal Kumar <ujwalkumar95@gmail.com>
2023-08-06 00:15:04 +0530

    Add auth middleware

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -1,28 +1,31 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import * as jose from 'jose';
 import jwt from 'jsonwebtoken';
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const bearerToken = req.headers["authorization"] as string;
-    if (!bearerToken){
-        return res.status(401).json({errorMessage: "Unauthorized request"});
-    }
-
     const token = bearerToken.split(" ")[1];
+    const payload = jwt.decode(token) as {email: string};
 
-    if (!token){
+    if (!payload.email){
         return res.status(401).json({errorMessage: "Unauthorized request"});
     }
 
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-
-    try {
-        await jose.jwtVerify(token, secret);
-    }
-    catch (error) {
-        return res.status(401).json({errorMessage: "Unauthorized request"});
-    }
-
-
-    return res.json({me: "test"})
+    const user = await prisma.user.findFirst({
+        where: {
+            email: payload.email
+        },
+        select: {
+            id: true,
+            first_name: true,
+            last_name: true,
+            email: true,
+            city: true,
+            phone: true
+        }
+    })
+    return res.json({ user })
 }

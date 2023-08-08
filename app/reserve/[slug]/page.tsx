d1e31e990f21171862a71caf361e417c3f6025ab
Ujwal Kumar <ujwalkumar95@gmail.com>
2023-08-08 23:33:03 +0530

    Add query params info on reservation page

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,6 +1,8 @@
+import { PrismaClient } from "@prisma/client"
 import Form from "../../../components/reserve/Form"
 import Header from "../../../components/reserve/Header"
 import { convertSlugToTitle } from "../../../utilities/convertSlugToTitle"
+import { notFound } from "next/navigation"
 
 export async function generateMetadata({ params }: { params: { slug: string } }) {
     return {
@@ -8,11 +10,31 @@ export async function generateMetadata({ params }: { params: { slug: string } })
     }
 }
 
-export default function Reserve(){
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string) => {
+    const restaurant = await prisma.restaurant.findUnique({
+        where: {
+            slug
+        }
+    });
+
+    if (!restaurant){
+        notFound();
+    }
+
+    return restaurant;
+}
+
+export default async function Reserve({params, searchParams}: {
+    params: {slug: string},
+    searchParams: {date: string, partySize: string}
+}){
+    const restaurant = await fetchRestaurantBySlug(params.slug);
     return (
         <div className='border-t h-screen'>
             <div className='py-9 w-3/5 m-auto'>
-                <Header/>
+                <Header image={restaurant.main_image} date={searchParams.date} partySize={searchParams.partySize} name={restaurant.name}/>
                 <Form/>
             </div>
         </div>
--- components/reserve/Header.tsx
+++ components/reserve/Header.tsx
@@ -1,17 +1,23 @@
-const Header = () => {
+import { Time, convertToDisplayTime } from "../../utilities/convertToDisplayTime";
+import { format } from 'date-fns'
+
+const Header = ({name, image, date, partySize}: {name: string, image: string, date: string, partySize: string}) => {
+    const [day, time] = date.split('T');
   return (
     <div>
         <h3 className='font-bold'>You&apos;re almost done!</h3>
         <div className='mt-5 flex'>
-            <img src='https://images.otstatic.com/prod/28353375/2/small.jpg' alt='' className='w-32 h-18 rounder '></img>
+            <img src={image} alt='' className='w-32 h-18 rounder '></img>
             <div className='ml-4'>
                 <h1 className='text-3xl font-bold'>
-                    Aiana Restaurant Collective
+                    {name}
                 </h1>
                 <div className='flex mt-3'>
-                    <p className='mr-6'>Tues, 22, 2023</p>
-                    <p className='mr-6'>7:30 PM</p>
-                    <p className='mr-6'>3 People</p>
+                    <p className='mr-6'>
+                        {format(new Date(date), "ccc, LLL d")}
+                    </p>
+                    <p className='mr-6'>{convertToDisplayTime(time as Time)}</p>
+                    <p className='mr-6'>{partySize} {parseInt(partySize) === 1 ? 'person' : 'persons'}</p>
                 </div>
             </div>
         </div>
